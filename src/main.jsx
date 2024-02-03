037ac56362566a706c6d63eb680f6056c538f37c
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2024-02-03 14:09:43 -0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />); // React is goign to hook into a single div on our HTML (root) and populate it with conent in App
